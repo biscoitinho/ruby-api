caadb726e6c759ec8fafdffd0ae847053d3e3d81
Mateusz <biscoitinho@o2.pl>
2017-04-19 14:12:56 +0200

    add self create table when non-existent

--- config/database.rb
+++ config/database.rb
@@ -3,3 +3,9 @@ ActiveRecord::Base.establish_connection(
   :database =>  './db/api.db'
 )
 
+unless ActiveRecord::Base.connection.data_source_exists?(:games)
+  ActiveRecord::Base.connection.create_table :games do |t|
+    t.string :name
+    t.string :genre
+  end
+end
